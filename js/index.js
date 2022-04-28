49a8a44813ddfe11aa770d976629906f96d0baf8
FherAmableRicse <amablericsefher@gmail.com>
2022-04-27 23:03:36 -0500

    JS

---
+++ js/index.js
@@ -0,0 +1,24 @@
+'use strict';
+const documentReady = () => {
+    const  heroTitle=  document.querySelector('.hero__title');
+    const  heroButtonSi=  document.getElementById('heroButtonSi');
+    const  heroButtonNo=  document.querySelector('#heroButtonNo');
+    
+    const nowZomosNobios = () => {
+        alert('💞Now zomos ParTnEr 💞');
+        alert('😱Nuestra VoDa Is MañaNa😱');
+        location.href ='https://www.youtube.com/watch?v=17z3Y1YnRTs';
+    };
+    const evitarKCRompanMyBobo = () => {
+        heroButtonNo.style.position='absolute';
+        heroButtonNo.style.top= (Math.random() * window.innerHeight) + 'px';
+        heroButtonNo.style.left= (Math.random() * window.innerWidth) + 'px';
+    };
+    
+    const partner = prompt('🥰 Dyme 2 naMe 🥰 ');
+    heroTitle.innerHTML  += partner + '💗';
+
+    heroButtonSi.addEventListener('click', nowZomosNobios);
+    heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+document.addEventListener('DOMContentLoaded',documentReady);
